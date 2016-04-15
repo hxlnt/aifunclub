5c2b010737c0034f792d91c06789ca00bf886f94
Rachel Simone Weil <partytimehexcellent@gmail.com>
2016-04-14 20:04:30 -0500

    Tryna change glasses size and landing spot now

--- public/js/dealwithit.js
+++ public/js/dealwithit.js
@@ -34,10 +34,9 @@ socket.on('face', function(response) {
                     style: 'transform-origin: 50% 0%; top: 0px; transform: rotateY(180deg) rotateZ(' + (-1 * face.faceAttributes.headPose.roll) + 'deg); width:' + (1.35 * face.faceRectangle.width) + 'px; left:' + (centerofeyes - 0.35 * (1.35 * face.faceRectangle.width)) + 'px; height:' + (1.2 * 0.15 * face.faceRectangle.width) + 'px;'
                 }).appendTo('#ontop');
             }
-            
+           
             imgResize();
             
-            var landingspot = (face.faceLandmarks.pupilLeft.y + face.faceLandmarks.pupilRight.y) / 2 - (parseInt($(".glasses").eq(i).css('height'), 10) / 2.2);
             $(".glasses").eq(i).animate({ top: (landingspot) }, 1800, function() {
                 if (parseInt($(".glasses").eq(-1).css('top'), 10) >= Math.trunc(landingspot)) {
                     $('#text').html('DEAL WITH IT');
@@ -73,5 +72,8 @@ function imgResize(){
      $('#text').css("font-size", imgwidth/10 + "px");
      $('#text').css("top", (imgheight - (imgwidth/10)) + "px");
      $('.glasses').width = scalar * $('.glasses').width;
+     
+     var landingspot = scalar * (face.faceLandmarks.pupilLeft.y + face.faceLandmarks.pupilRight.y) / 2 - (parseInt($(".glasses").eq(i).css('height'), 10) / 2.2);
+     
 });    
 }
