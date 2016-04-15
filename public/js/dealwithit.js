4db88c632cbb21ca48e883f4f9dbbf6ad3c13fc9
Rachel Simone Weil <partytimehexcellent@gmail.com>
2016-04-14 19:54:52 -0500

    First attempt at image resizing

--- public/js/dealwithit.js
+++ public/js/dealwithit.js
@@ -17,7 +17,6 @@ socket.on('face', function(response) {
         location.reload(true);
     }
     else {
-        //jQuery('<img>', { src: origimgurl }).appendTo('#container');
         $('<img>', { src: origimgurl } ).appendTo( '#container' );
         for (i = 0; i < response.length; i++) {
             face = response[i];
@@ -35,6 +34,9 @@ socket.on('face', function(response) {
                     style: 'transform-origin: 50% 0%; top: 0px; transform: rotateY(180deg) rotateZ(' + (-1 * face.faceAttributes.headPose.roll) + 'deg); width:' + (1.35 * face.faceRectangle.width) + 'px; left:' + (centerofeyes - 0.35 * (1.35 * face.faceRectangle.width)) + 'px; height:' + (1.2 * 0.15 * face.faceRectangle.width) + 'px;'
                 }).appendTo('#ontop');
             }
+            
+            imgResize();
+            
             var landingspot = (face.faceLandmarks.pupilLeft.y + face.faceLandmarks.pupilRight.y) / 2 - (parseInt($(".glasses").eq(i).css('height'), 10) / 2.2);
             $(".glasses").eq(i).animate({ top: (landingspot) }, 1800, function() {
                 if (parseInt($(".glasses").eq(-1).css('top'), 10) >= Math.trunc(landingspot)) {
@@ -46,3 +48,30 @@ socket.on('face', function(response) {
         }
     }
 });
+
+function imgResize(){
+    $( 'img' ).load(function(){
+    var scalar = 1;
+    var imgwidth = (scalar * $('img').width());
+    var imgheight = (scalar * $('img').height());
+    
+    if ( $( '#container' ).height() >= imgheight && $( '#text' ).width() >= imgwidth ) {
+        scalar = 1;
+        }
+        
+    else if ( $( '#text' ).width() < imgwidth ) {
+        scalar = ($( '#text' ).width() / imgwidth);   
+        }
+        
+    else if ( ($( '#container' ).height() < imgheight) ) {
+        scalar = $( '#container' ).height() / imgheight;   
+        }
+        
+     imgwidth = scalar * imgwidth;   
+     imgheight = scalar * imgheight;
+     $('img').css("height", imgheight + 'px');
+     $('#text').css("font-size", imgwidth/10 + "px");
+     $('#text').css("top", (imgheight - (imgwidth/10)) + "px");
+     $('#glasses').width = scalar * $('#glasses').width;
+});    
+}
