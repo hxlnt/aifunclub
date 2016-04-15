7bfee3d10b00baeecc55f499f3d5eef2af938138
Rachel Simone Weil <partytimehexcellent@gmail.com>
2016-04-14 21:29:09 -0500

    Parentheses at it again!

--- public/js/dealwithit.js
+++ public/js/dealwithit.js
@@ -61,12 +61,11 @@ socket.on('face', function(response) {
                     }).appendTo('#ontop');
                 }
                 
-                var landingspot = scalar * 
+                var landingspot = (scalar * 
                 (
                     ((face.faceLandmarks.pupilLeft.y + face.faceLandmarks.pupilRight.y) / 2) - 
-                    (parseInt($(".glasses").eq(i).css('height'), 10) / 2) + 
-                    $('#container').css("padding-top") 
-                );
+                    (parseInt($(".glasses").eq(i).css('height'), 10) / 2) 
+                )) + $('#container').css("padding-top");
                 
                 $(".glasses").eq(i).animate({ top: (landingspot) }, 1800, function() {
                     if (parseInt($(".glasses").eq(-1).css('top'), 10) >= Math.trunc(landingspot)) {
